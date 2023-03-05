2e1e10c063c79953bcce84a8968f1caf55f99542
justTrueCodeWriter <>
2023-03-05 15:02:35 +0300

    upd

--- lab17/main.cpp
+++ lab17/main.cpp
@@ -5,33 +5,35 @@
 
 void rows_cols_scan(int &rows, int &cols);
 
-int fill_mode(int arr[ROWS][COLS], int rows, int cols);
-int output_mode(int arr[ROWS][COLS], int rows, int cols);
+int fill_mode(double arr[ROWS][COLS], int &mainRows, int &mainCols);
+int output_mode(double arr[ROWS][COLS], int rows, int cols);
 
-void user_array_filler(int arr[ROWS][COLS], int rows, int cols); 
-int array_reader_txt(int arr[ROWS][COLS], char *fileName);
-int array_reader_binary(int arr[ROWS][COLS], char *fileName);
-void random_array_filler(int arr[ROWS][COLS], int rows, int cols);
+void user_array_filler(double arr[ROWS][COLS], int rows, int cols); 
+int array_reader_txt(double arr[ROWS][COLS], int &rows, int &cols, char *fileName);
+int array_reader_binary(double arr[ROWS][COLS], int &rows, int &cols, char *fileName);
+void random_array_filler(double arr[ROWS][COLS], int rows, int cols);
 double formula(int i, int j);
-void formula_array_filler(int arr[ROWS][COLS], int rows, int cols);
+void formula_array_filler(double arr[ROWS][COLS], int rows, int cols);
 
-void screen_array_printer(int arr[ROWS][COLS], int rows, int cols);
-int array_saver_txt(int arr[ROWS][COLS], int rows, int cols, char *fileName);
-int array_saver_binary(int arr[ROWS][COLS], int rows, int cols, char *fileName);
+void screen_array_printer(double arr[ROWS][COLS], int rows, int cols);
+int array_saver_txt(double arr[ROWS][COLS], int rows, int cols, char *fileName);
+int array_saver_binary(double arr[ROWS][COLS], int rows, int cols, char *fileName);
 
 int main() {
 
-	int arr[ROWS][COLS];
+	double arr[ROWS][COLS];
 	int errorStatus, outputCheck;
-	int rows, cols;	
+	int mainRows, mainCols;	
 
-	rows_cols_scan(rows, cols);
 
-	errorStatus = fill_mode(arr, rows, cols);
+	errorStatus = fill_mode(arr, mainRows, mainCols);
+	if (errorStatus == -1) {
+		return -1;
+	}
 
-	screen_array_printer(arr, rows, cols);
+	screen_array_printer(arr, mainRows, mainCols);
 
-	output_mode(arr, rows, cols);
+	output_mode(arr, mainRows, mainCols);
 	
 	return 0;
 
@@ -40,50 +42,57 @@ int main() {
 void rows_cols_scan(int &rows, int &cols) {
 
 	do {
-		printf("rows"); scanf("%d", &rows);		
-		printf("cols"); scanf("%d", &cols);		
+		printf("rows "); scanf("%d", &rows);		
+		printf("cols "); scanf("%d", &cols);		
 		if ((rows<1)||(cols<1))
 			printf("INCORRECT: rows and cols must satisfy (rows > 0 and cols > 0)");
 	}while((rows<1)||(cols<1));
 
 }
 
-int fill_mode(int arr[ROWS][COLS], int rows, int cols){
+int fill_mode(double arr[ROWS][COLS], int &mainRows, int &mainCols){
 
-	int userChoice, errorStatus=0;
+	int userChoice, errorStatus=0, rows, cols;
 	char textFileName[]="src.txt", binFileName[]="src.bin";
 // FILL MODE
 	do {
 		printf("Select the array fill mode:\nUser input(1)\nArray from text file(2)\nArray from binary file(3)\nRandom fill(4)\nFormula fill(5)\n> "); 
 		scanf("%d", &userChoice);
 		switch (userChoice) {
-			case 1: user_array_filler(arr, rows, cols);
+			case 1: rows_cols_scan(rows, cols);
+					user_array_filler(arr, rows, cols);
 					break;
-			case 2: errorStatus = array_reader_txt(arr, textFileName);	
+			case 2: errorStatus = array_reader_txt(arr, rows, cols, textFileName);	
 					break;
-			case 3: errorStatus = array_reader_binary(arr, binFileName);
+			case 3: errorStatus = array_reader_binary(arr, rows, cols, binFileName);
 					break;
-			case 4: random_array_filler(arr, rows, cols);
+			case 4: rows_cols_scan(rows, cols);
+					random_array_filler(arr, rows, cols);
 					break;
-			case 5: formula_array_filler(arr, rows, cols); 
+			case 5: rows_cols_scan(rows, cols);
+					formula_array_filler(arr, rows, cols); 
 					break;
-			default: printf("Mode must satisfy (1 <= choice <= 5)\n");
+			default: printf("Mode must satisfy (1 <= choice <= 5)\n"); 
+					 errorStatus = -1;
 		}
-	}while((userChoice < 1)&&(userChoice > 5));
+	}while((userChoice < 1)||(userChoice > 5));
 
 	if (errorStatus == -1)
 		return -1;
 
+	mainRows = rows;
+	mainCols = cols;
+
 	return 0;
 }
-int output_mode(int arr[ROWS][COLS], int rows, int cols){
+int output_mode(double arr[ROWS][COLS], int rows, int cols){
 
 	int userChoice, errorStatus;
 
 	char textFileName[]="src.txt", binFileName[]="src.bin";
 // OUTPUT MODE
 	do {
-		printf("Select the array output mode:\nPrint on screen(1)\nSave to text file(2)\nSave to binary file(3)\n> "); 
+		printf("Select the array output mode:\nSave to text file(1)\nSave to binary file(2)\n> "); 
 		scanf("%d", &userChoice);
 		switch (userChoice) {
 			case 1: errorStatus  = array_saver_txt(arr, rows, cols, textFileName);	
@@ -101,31 +110,63 @@ int output_mode(int arr[ROWS][COLS], int rows, int cols){
 }
 
 // FILLING BY USER
-void user_array_filler(int arr[ROWS][COLS], int rows, int cols) {
+void user_array_filler(double arr[ROWS][COLS], int rows, int cols) {
 
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < cols; j++) {
-			printf(""); scanf("%d", &arr[i][j]);
+			printf(": "); scanf("%lf", &arr[i][j]);
 		}
 	}
 
 }
 
-int array_reader_txt(int arr[ROWS][COLS], char *fileName) {
+int array_reader_txt(double arr[ROWS][COLS], int &rows, int &cols, char *fileName) {
+	FILE *ft;
+	ft=fopen(fileName,"rt");	
 
-	printf("array_reader_txt");
+	if (ft == NULL) {
+		printf("ERROR: CANT'T FIND SOURCE FILE\n");
+		return -1;
+	}
+
+	fscanf(ft, "%d %d", &rows, &cols);
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			fscanf(ft, "%lf", &arr[i][j]);
+		}
+	}
+		
+	fclose(ft);
+
 	return 0;
 
 }
 
-int array_reader_binary(int arr[ROWS][COLS], char *fileName) {
+int array_reader_binary(double arr[ROWS][COLS], int &rows, int &cols, char *fileName) {
+	
+	FILE *ft;
+	ft=fopen(fileName,"rb");	
 
+	if (ft == NULL) {
+		printf("ERROR: CANT'T FIND SOURCE FILE\n");
+		return -1;
+	}
 
-	printf("array_reader_binary");
+	fread(&rows, sizeof(int), 1, ft);
+	fread(&cols, sizeof(int), 1, ft);
+
+	for (int i = 0; i < rows; i++) {
+			fread(&arr[i][0], sizeof(double), cols, ft);
+	}
+		
+	fclose(ft);
+
 	return 0;
+
 }
 
-void random_array_filler(int arr[ROWS][COLS], int rows, int cols) {
+void random_array_filler(double arr[ROWS][COLS], int rows, int cols) {
 
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < cols; j++) {
@@ -148,52 +189,67 @@ double formula(int i, int j) {
 
 }
 
-void formula_array_filler(int arr[ROWS][COLS], int rows, int cols) {
+void formula_array_filler(double arr[ROWS][COLS], int rows, int cols) {
 
-	for (int i = 0; i < rows; i++) {
-		for (int j = 0; j < cols; j++) {
+	for (int i = 1; i < rows; i++) {
+		for (int j = 1; j < cols; j++) {
 			arr[i][j] = formula(i, j);
 		}
 	}
 
 }
 
-void screen_array_printer(int arr[ROWS][COLS], int rows, int cols) {
+void screen_array_printer(double arr[ROWS][COLS], int rows, int cols) {
 
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < cols; j++) {
-			printf("%d ", arr[i][j]);
+			printf("%lf ", arr[i][j]);
 		}
 		printf("\n");
 	}
 }
 
-int array_saver_txt(int arr[ROWS][COLS], int rows, int cols, char *fileName) {
+int array_saver_txt(double arr[ROWS][COLS], int rows, int cols, char *fileName) {
 
 	FILE *ft;
 	ft=fopen(fileName,"wt");	
 
-	if (ft == 0) {
+	if (ft == NULL) {
 		printf("ERROR: CANT'T FIND SOURCE FILE\n");
-		return 0;
+		return -1;
 	}
 
-	fprintf(ft, "%d %d", rows, cols);
+	fprintf(ft, "%d %d\n", rows, cols);
 
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < cols; j++) {
-			fprintf(ft, "%d", arr[i][j]);
+			fprintf(ft, "%lf ", arr[i][j]);
 		}
-		fprintf(ft, "%d", arr[i][j]);
+		fprintf(ft, "\n");
 	}
 		
 	fclose(ft);
 
-	printf("array_saver_txt");
+	return 0;
 }
-int array_saver_binary(int arr[ROWS][COLS], int rows, int cols, char *fileName) {
+int array_saver_binary(double arr[ROWS][COLS], int rows, int cols, char *fileName) {
 
+	FILE *ft;
+	ft=fopen(fileName,"wb");	
 
-	printf("array_saver_binary");
+	if (ft == NULL) {
+		printf("ERROR: CANT'T FIND SOURCE FILE\n");
+		return -1;
+	}
+
+	fwrite(&rows, sizeof(int), 1, ft);
+	fwrite(&cols, sizeof(int), 1, ft);
+
+	for (int i = 0; i < rows; i++) {
+			fwrite(&arr[i][0], sizeof(double), cols, ft);
+	}
+		
+	fclose(ft);
+
 	return 0;
 }
